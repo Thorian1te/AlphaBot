510d26afb37552cde36212ac92377b876c5d1110
Thorianite <100335276+Thorian1te@users.noreply.github.com>
2023-07-11 09:33:27 +0930

    removed sma from five minute direction

--- src/tradingIndicators.ts
+++ src/tradingIndicators.ts
@@ -570,7 +570,7 @@ export class TradingIndicators {
           trade.tradeType = TradingMode.sell;
           return trade
         }
-        if (detectTop.isTrendReversal && detectRsiTop.isTrendReversal && fiveMinuteDirection !== 'Upward' && lastRsi >=50 && lastPrice > lastTradePrice) {
+        if (detectTop.isTrendReversal && detectRsiTop.isTrendReversal && fiveMinuteDirection !== 'Upward' && lastRsi >=50 ) {
           trade.tradeSignal = "sell: Price approaching resistance level and top detected";
           trade.tradeType = TradingMode.sell;
           return trade
